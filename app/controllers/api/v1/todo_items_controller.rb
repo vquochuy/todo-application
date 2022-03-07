cf7787094574313da01db03d42d32b238b401a7c
Vu Quoc Huy <huy.vuquoc@axonactive.com>
2022-03-07 20:54:22 +0700

    add create, update, delete

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -5,12 +5,50 @@ class Api::V1::TodoItemsController < ApplicationController
         @todo_items = current_user.todo_items.all
     end
     def show
+        if authorized?
+            respond_to do |format|
+              format.json { render :show }
+            end
+        else
+            handle_unauthorized
+        end
     end
     def create
+        @todo_item = current_user.todo_items.build(todo_item_params)
+      if authorized?
+        respond_to do |format|
+          if @todo_item.save
+            format.json { render :show, status: :created, location: api_v1_todo_item_path(@todo_item) }
+          else
+            format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        handle_unauthorized
+      end
     end
     def update
+        if authorized?
+            respond_to do |format|
+              if @todo_item.update(todo_item_params)
+                format.json { render :show, status: :ok, location: api_v1_todo_item_path(@todo_item) }
+              else
+                format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+              end
+            end
+        else
+            handle_unauthorized
+        end
     end
     def destroy
+        if authorized?
+            @todo_item.destroy
+            respond_to do |format|
+              format.json { head :no_content }
+            end
+        else
+            handle_unauthorized
+        end
     end
     private
         def set_todo_item
@@ -27,5 +65,9 @@ class Api::V1::TodoItemsController < ApplicationController
                 format.json { render :unauthorized, status: 401 }
               end
             end
+        end
+
+        def todo_item_params
+            params.require(:todo_item).permit(:title, :complete)
         end
 end
