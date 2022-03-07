1971e4c268182272df856cd671b264997b858710
Vu Quoc Huy <huy.vuquoc@axonactive.com>
2022-03-07 13:16:23 +0700

    add view login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
-  get 'pages/my_todo_items'
+  authenticated :user do
+    root "pages#my_todo_items", as: :authenticated_root
+  end
+  root "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
