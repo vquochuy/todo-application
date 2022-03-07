aaed55c4a3eccac25fe45419d9e1941ef290c5e4
Vu Quoc Huy <huy.vuquoc@axonactive.com>
2022-03-07 16:40:10 +0700

    Create non authorized end points

---
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::TodoItemsController < ApplicationController
+    before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+    def index
+        @todo_items = TodoItem.all
+    end
+    def show
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
+    private
+        def set_todo_item
+            @todo_item = TodoItem.find(params[:id])
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     root "pages#my_todo_items", as: :authenticated_root
   end
   root "pages#home"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
